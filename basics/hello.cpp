f2d8f3f24aa5c77d00045f15b7a2170e347376fe
jubayerweb <showkatjubayer@gmail.com>
2022-05-31 10:58:16 +0600

    setup

---
+++ basics/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    cout << "Hello, World!" << endl;
+    cout << "I am Jubayer" << endl;
+    return 0;
+}
