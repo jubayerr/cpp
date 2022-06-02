f1f5896a20d6b2aa2bc6bdad6faa7113c4392f3e
jubayerweb <showkatjubayer@gmail.com>
2022-06-02 12:22:39 +0600

    variable

---
+++ basics/var.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /* code */
+    string characterName = "Jubayer";
+    int characterAge;
+    characterAge = 25;
+
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was " << characterAge <<  " years old" << endl;
+    cout << "He liked the name " << characterName << endl;
+    cout << "But did not like being " << characterAge << endl;
+
+    return 0;
+}
