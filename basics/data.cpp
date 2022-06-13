84379fb6101303ab14646887e04bb70c5d51c6d9
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2022-06-13 08:53:47 +0600

    data types

---
+++ basics/data.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+
+    char grade = 'A';
+    string phrase = "Jubayer Web";
+    int age = 25;
+    double gpa = 3.5;
+    bool isMale = true;
+
+    cout << grade << endl;
+    cout << phrase << endl;
+    cout << age << endl;
+    cout << gpa << endl;
+
+    return 0;
+}
