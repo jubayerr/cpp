c27872bf7c8c8cc592247ed18354705e0b95904c
jubayerweb <showkatjubayer@gmail.com>
2022-07-01 22:18:02 +0600

    rough c++

---
+++ basics/rough.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int A, B;
+    
+    cout << "input number: ";
+    cin >> A;
+
+    cout << "input any number: ";
+    cin >> B;
+    
+    int X = A + B;
+    
+    cout << "X = " << X << endl;
+ 
+    return 0;
+}
