03321ee9c94fb26c9939058f8d8036af9467e36b
jubayerweb <showkatjubayer@gmail.com>
2022-07-02 19:19:04 +0600

    classes and objects

---
+++ basics/classes.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+// classes and objects
+
+class Book {
+    public:
+    string title;
+    string author;
+    int pages;
+};
+
+int main()
+{
+    Book book1;
+
+    book1.title = "Harry Potter";
+    book1.author = "JK Rowling";
+    book1.pages = 500;
+
+    Book book2;
+
+    book2.title = "Lord of the Ring";
+    book2.author = "Tolkein";
+    book2.pages = 700;
+
+    cout << book1.title << endl;
+    cout << book2.title << endl;
+
+    return 0;
+}
