507de57d3277ec321a38a60abed1a3bd94718fc5
jubayerweb <showkatjubayer@gmail.com>
2022-07-02 20:05:56 +0600

    constructor

--- basics/classes.cpp
+++ basics/classes.cpp
@@ -8,21 +8,18 @@ class Book {
     string title;
     string author;
     int pages;
+    Book(string aTitle, string aAuthor, int aPages){
+        title = aTitle;
+        author = aAuthor;
+        pages = aPages;
+    }
 };
 
 int main()
 {
-    Book book1;
+    Book book1("Harry Potter", "JK Rowling", 500);
 
-    book1.title = "Harry Potter";
-    book1.author = "JK Rowling";
-    book1.pages = 500;
-
-    Book book2;
-
-    book2.title = "Lord of the Ring";
-    book2.author = "Tolkein";
-    book2.pages = 700;
+    Book book2("Lord of the Ring", "Tolkein", 700);
 
     cout << book1.title << endl;
     cout << book2.title << endl;
