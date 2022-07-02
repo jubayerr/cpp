0618f3a1acd60822f5ce14aa64ce86f1708ccd0f
jubayerweb <showkatjubayer@gmail.com>
2022-07-02 12:15:41 +0600

    foor loop

---
+++ basics/forloop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// for loop
+
+int main()
+{
+
+    int nums[] = {2, 4, 5, 6, 8, 2, 3};
+
+    for(int i = 0; i <= 6; i++)
+    {
+        cout << nums[i] << endl;
+    }
+
+    return 0;
+}
