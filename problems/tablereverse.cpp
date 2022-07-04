16847217402d05e8ebd93c654ea3a79af7c6b7a7
jubayerweb <showkatjubayer@gmail.com>
2022-07-04 19:42:09 +0600

    reverse table

---
+++ problems/tablereverse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+// reverse the table
+
+int main()
+{
+    int i;
+
+    for ( i = 20; i >= 2; i-=2)
+    {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
