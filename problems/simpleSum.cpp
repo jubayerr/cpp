0a7a2bb68d02650239816b05b9815abe064cfc0b
jubayerweb <showkatjubayer@gmail.com>
2022-07-04 11:18:27 +0600

    simple sum

---
+++ problems/simpleSum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// beCrowd 1003 - Simple Sum
+
+int main()
+{
+   int A, B, SOMA;
+
+   cin >> A >> B;
+
+   SOMA = A + B;
+
+   cout << "SOMA = " << SOMA << endl;
+
+    return 0;
+}
