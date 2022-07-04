93066c7182e5dc2d7c53e88598200aa41426f640
jubayerweb <showkatjubayer@gmail.com>
2022-07-04 19:27:01 +0600

    print 1 to 100

---
+++ problems/print1to100.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// print 1 to 100
+
+int main()
+{
+
+    int i;
+
+    for ( i = 0; i <= 100; i++)
+    {   
+        cout << i << endl;
+    }
+
+    return 0;
+}
