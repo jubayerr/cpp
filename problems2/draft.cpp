66cbc3d81e448d8b7dcc492a37aa0e1ad29dd651
jubayerweb <showkatjubayer@gmail.com>
2022-07-07 09:12:36 +0600

    fibo series

--- problems2/draft.cpp
+++ problems2/draft.cpp
@@ -3,17 +3,20 @@ using namespace std;
 
 int main()
 {
-    int i, f, n;
+    int i, n3, n, n1 = 0, n2 = 1;
 
     cout << "Enter a number: ";
     cin >> n; 
 
     for (i = 1; i <= n; i++)
     {
-        f = f * i;
-    }
+        n3 = n2 + n1;
+        n1 = n2;
+        n2 = n3;
 
-    cout << "Factorial of " << n << " = " << f << endl;
+        cout << n3 << endl;
+    }
+    
     
     return 0;
 }
