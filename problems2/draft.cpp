833a2977fa282fd0ccf13e48af1e8c70a418e8cc
jubayerweb <showkatjubayer@gmail.com>
2022-07-08 08:56:56 +0600

    practice problem solving

--- problems2/draft.cpp
+++ problems2/draft.cpp
@@ -3,19 +3,17 @@ using namespace std;
 
 int main()
 {
-    int i, n3, n, n1 = 0, n2 = 1;
+    int i, f, n;
 
     cout << "Enter a number: ";
     cin >> n; 
 
-    for (i = 1; i <= n; i++)
+    for (i = 1; i < n; i++)
     {
-        n3 = n2 + n1;
-        n1 = n2;
-        n2 = n3;
-
-        cout << n3 << endl;
+        f = f * i;
     }
+    
+    cout << "!" << n << " = " << f << endl;
     
     
     return 0;
