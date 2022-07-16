669ffe2805ae601de751321762dc32782178a20d
jubayerweb <showkatjubayer@gmail.com>
2022-07-16 18:03:29 +0600

    check leap year

---
+++ problems2/leapYear.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout << "Enter a year: ";
+    cin >> year;
+
+    if (year % 400 == 0)
+    {
+        cout << "This is a leap year." << endl;
+    }
+    else if (year % 100 == 0)
+    {
+        cout << "This is not a leap year." << endl;
+    }
+    else if (year % 4 == 0)
+    {
+        cout << "This is a leap year." << endl;
+    }
+    else 
+    {
+        cout << "This is not a leap year." << endl;
+    }
+    
+
+    return 0;
+}
